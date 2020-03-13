b93924a28098b2e20b823e43fa3b7d131fed5aa8
jorge_araujo <araujojr.jorge@gmail.com>
2020-03-12 23:34:21 -0500

    Configure Castle.Windsor controller & persistence installers; Configure nHibernate persistence facility

---
+++ Telegenic.Web/Plumbing/CastleWindsor/Installers/PersistenceInstaller.cs
@@ -0,0 +1,15 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Telegenic.Web.Plumbing.nHibernate;
+
+namespace Telegenic.Web.Plumbing.CastleWindsor.Installers
+{
+    public class PersistenceInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.AddFacility<PersistenceFacility>();
+        }
+    }
+}
